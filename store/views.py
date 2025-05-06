a4e82bc21a59daaefd449503dc4cba0aac2b2899
sreng0102 <natkimsreng@gmail.com>
2025-05-06 15:38:31 +0700

    add checkout

--- store/views.py
+++ store/views.py
@@ -68,16 +68,7 @@ def store(request):
 	return render(request, 'store/store.html', context)
 
 def checkout(request):
-	if request.user.is_authenticated:
-		customer = request.user.customer
-		order, created = Order.objects.get_or_create(customer=customer, complete=False)
-		items = order.orderitem_set.all()
-	else:
-		#Create empty cart for now for non-logged in user
-		items = []
-		order = {'get_cart_total':0, 'get_cart_items':0}
-
-	context = {'items':items, 'order':order}
+	context = {}
 	return render(request, 'store/checkout.html', context)
 
 def loginPage(request):
