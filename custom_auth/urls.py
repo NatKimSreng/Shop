32b107f20642326012657398260c0226712a0b14
sreng0102 <natkimsreng@gmail.com>
2025-06-06 12:03:07 +0700

    add dashboard

--- custom_auth/urls.py
+++ custom_auth/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.admin_dashboard, name='admin_dashboard'),
+     path('admin/products/', views.admin_product_list, name='admin_product_list'),
 ]
--- custom_auth/views.py
+++ custom_auth/views.py
@@ -1,14 +1,16 @@
-from django.shortcuts import render
-
-# Create your views here.
-from django.contrib.auth.decorators import login_required
-from django.contrib.auth import login, authenticate
-from django.contrib.auth.forms import UserCreationForm
-from django.shortcuts import redirect
-from django.urls import reverse_lazy
+from django.contrib.auth.decorators import login_required, user_passes_test
 from django.shortcuts import render
 from store.models import Product
 
-@login_required
+# Check if user is admin or superuser
+def is_admin_or_superuser(user):
+    return user.is_authenticated and (user.is_staff or user.is_superuser)
+
+@login_required(login_url='../login/')
+@user_passes_test(is_admin_or_superuser, login_url='../login/')
 def admin_dashboard(request):
     return render(request, 'admin_dashboard.html')
+
+def admin_product_list(request):
+    products = Product.objects.all()
+    return render(request, 'admin_product_list.html', {'products': products})
